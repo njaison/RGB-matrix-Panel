3c31b04f9043d456cbb1a44fe06aba764e2a7734
njaison <nidhijaison@gmail.com>
2018-12-26 11:35:03 -0500

    added 32x64 constructor

--- RGBmatrixPanel.cpp
+++ RGBmatrixPanel.cpp
@@ -119,7 +119,7 @@ void RGBmatrixPanel::init(uint8_t rows, uint8_t a, uint8_t b, uint8_t c,
   addrbport = portOutputRegister(digitalPinToPort(b));
   addrbpin  = digitalPinToBitMask(b);
   addrcport = portOutputRegister(digitalPinToPort(c));
-  addrcpin  = digitalPinToBitMask(c); 
+  addrcpin  = digitalPinToBitMask(c);
   nPlanes   = 4;     // Other code is fixed at 4 planes; don't change this
   plane     = nPlanes - 1;
   row       = nRows   - 1;
@@ -145,6 +145,23 @@ RGBmatrixPanel::RGBmatrixPanel(
   init(16, a, b, c, sclk, latch, oe, dbuf, pwidth);
 
   // Init a few extra 32x32-specific elements:
+  _d        = d;
+  addrdport = portOutputRegister(digitalPinToPort(d));
+  addrdpin  = digitalPinToBitMask(d);
+}
+
+
+// Constructor for 32x64 panel:
+RGBmatrixPanel::RGBmatrixPanel(
+  uint8_t a, uint8_t b, uint8_t c, uint8_t d,
+  uint8_t sclk, uint8_t latch, uint8_t oe, boolean dbuf, uint8_t pwidth) :
+  Adafruit_GFX(64*pwidth, 32) {
+
+  pwidth *=2;
+
+  init(16, a, b, c, sclk, latch, oe, dbuf, pwidth);
+
+  // Init a few extra 32x64-specific elements:
   _d        = d;
   addrdport = portOutputRegister(digitalPinToPort(d));
   addrdpin  = digitalPinToBitMask(d);
@@ -275,7 +292,7 @@ void RGBmatrixPanel::drawPixel(int16_t x, int16_t y, uint16_t c) {
 
   switch(rotation) {
    case 1:
-    swap(x, y);
+ //   swap(x, y);
     x = WIDTH  - 1 - x;
     break;
    case 2:
@@ -283,7 +300,7 @@ void RGBmatrixPanel::drawPixel(int16_t x, int16_t y, uint16_t c) {
     y = HEIGHT - 1 - y;
     break;
    case 3:
-    swap(x, y);
+ //   swap(x, y);
     y = HEIGHT - 1 - y;
     break;
   }
